554e4c1a82000d011cc22291dfe504c49dfcb083
Nuno Facha <nunofacha@gmail.com>
2021-12-01 14:10:26 -0100

    WIP: Get tool temperature and additional data

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,24 @@
 let OCTOPRINT_VERSION = null;
 $().ready(async function () {
     OCTOPRINT_VERSION = await getVersion();
-    // debugger;
     console.log('Connected to Octoprint server version: ' + OCTOPRINT_VERSION.server);
-    console.log(await getCurrentJob());
+    setInterval(async function () {
+        updateOverlay();
+    }, 1000);
 });
+
+
+async function updateOverlay() {
+    //get job information
+    const data = await getCurrentJob();
+    const estimatedPrintTime = data.job.estimatedPrintTime;
+    const filamentLenghtUsed = data.job.filament.tool0.length;
+    const fileName = data.job.file.display;
+    const completionPercent = data.progress.completion;
+    const printTimeLeft = data.progress.printTimeLeft;
+    const printTime = data.progress.printTime;
+    const printState = data.state
+
+    //get printer information
+
+}
